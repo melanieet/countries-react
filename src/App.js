480d4e2f8aefa786b63a055746462b3b61b28c4a
melanieet <melanie@melanies-mbp.home>
2020-07-22 00:57:37 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import countriesAll from "./countriesAll.json";
 import CountryCard from "./CountryCard";
+import SearchBar from "./SearchBar";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect(() => {
+    const results = countriesAll.filter(
+      (country) =>
+        country.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        country.capital.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+
   return (
     <div>
       <div className="header">
         <h2 className="header-text">Where in the world?</h2>
       </div>
+      <div>
+        <SearchBar handleChange={handleChange} />
+      </div>
       <div className="all-countries">
-        {countriesAll.map((country) => {
-          return <CountryCard oneCountry={country} />;
+        {searchResults.map((country, index) => {
+          return <CountryCard oneCountry={country} key={index} />;
         })}
       </div>
     </div>
